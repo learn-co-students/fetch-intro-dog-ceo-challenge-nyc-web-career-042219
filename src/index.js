401daeadae99fd4896b1de861e5fefe0aac6136f
Pamyk <pamyk.ch@gmail.com>
2019-06-09 17:53:51 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,67 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+var allBreedsArr = [];
+
+window.addEventListener("DOMContentLoaded", (e) => {
+    
+    const dogContainer =document.querySelector("#dog-image-container")
+    const breedContainer = document.querySelector("#dog-breeds")
+    const breedSelector = document.querySelector("#breed-dropdown")
+    // console.log(breedContainer)
+
+
+    const dogImages = fetch(imgUrl)
+    .then(response => response.json())
+    .then(images => {
+
+        displayPictures(images.message)
+    })
+
+    function displayPictures(pictureArr){
+      return  dogContainer.innerHTML = pictureArr.map(pic => {
+            return `<img src=${pic} ></img>`
+        }).join(" ")
+    }
+
+    const dogBreeds = fetch(breedUrl)
+    .then(response => response.json())
+    .then(breeds => {
+        allBreedsArr = Object.keys(breeds.message);
+        displayBreeds(allBreedsArr)
+    
+    })
+
+
+    function displayBreeds(arr){
+        return breedContainer.innerHTML = arr.map(breed => {
+            return `<li>${breed}</li>`
+         } ).join(" ")
+
+    }
+
+    breedContainer.addEventListener('click', (e)=>{
+       
+        changeColors(e.target)
+    })
+
+    function changeColors(element){
+       let text = element.innerText 
+       element.innerHTML = `<li style = "color:#581845;"> ${text}</li>`
+
+    }
+
+    breedSelector.addEventListener('change', (e)=>{
+       filterBreeds(e.target.value)
+    })
+
+    function filterBreeds(letter){
+        let filteredBreeds = []
+        filteredBreeds = allBreedsArr.filter(breed => breed.charAt(0) === letter)
+        
+        displayBreeds(filteredBreeds);
+
+        
+    }
+})
+    
