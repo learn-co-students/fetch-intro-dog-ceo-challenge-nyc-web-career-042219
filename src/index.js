fb717ed749554827b2cf457096508d2c2aa03cc3
Jessica Lin <jessicaa.lin@gmail.com>
2019-06-08 20:14:59 -0400

    test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const imgContainer = document.querySelector('#dog-image-container')
+
+function addDogImg(url){
+    return `<img src=${imgUrl} height='200' width='300'>`
+}
+
+fetch(imgUrl) //fetch dog images
+    .then(res => res.json()) //res.json() is the promise
+    .then(data => { //data is the json, img URLs live under message key
+        data.message.map(url =>{
+            imgContainer.innerHTML += addDogImg(url)
+        }).join(' ');
+    })
