4b60756d3b7bc29a6257a046c53ac94e3fd568dd
Jennifer Oh <ohjennifer.90@gmail.com>
2019-06-10 19:48:15 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,42 @@
-console.log('%c HI', 'color: firebrick')
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+const breedUrl = "https://dog.ceo/api/breeds/list/all"
+const dogImage = document.getElementById("dog-image-container")
+const breedList = document.getElementById("dog-breeds")
+const dropDown = document.querySelector("#breed-dropdown")
+
+fetch(imgUrl)
+    .then(res => res.json())
+    .then(data => data.message.map(url => dogImage.innerHTML += `<img src=${url} height="300" width="400"><br>`))
+
+fetch(breedUrl)
+    .then(res => res.json())
+    .then(data => data.message)
+    .then(breed => {
+        for (let key in breed){
+            if (breed[key].length === 0){
+                breedList.innerHTML += `<li>${key}</li>`
+            } else {
+                breed[key].forEach(type => {
+                    breedList.innerHTML += `<li>${type} ${key}</li>`
+                })
+            }
+        }
+
+    })
+    
+breedList.addEventListener('click', (event) => {
+    event.target.style.color = "red";
+})
+
+dropDown.addEventListener('change', (event) => {
+    let domStr = ''
+    const array = Array.from(breedList.children)
+    array.filter(breed => {
+        return breed.innerText.startsWith(event.target.value)  
+    }).forEach(element => {
+        domStr += `<li>${element.innerHTML}</li>`
+    })
+    breedList.innerHTML = domStr
+})
+
+
